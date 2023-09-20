4576966e9872050eda98e7257929bef10c45886f
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-20 14:28:54 -0300

    Adicionando suporte a Razor Pages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages(); // adiciona suporte para páginas Razor
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapRazorPages(); // Auxilia no mapeamento das paginas 
 
 app.Run();
