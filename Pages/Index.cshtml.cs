52b61d019288ae3a97b7ae4ac86c66b6b3a7de7c
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-21 09:50:02 -0300

    Criando um Razor Page

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyRazorApp.Pages
+{
+    public class Index : PageModel
+    {
+        private readonly ILogger<Index> _logger;
+
+        public Index(ILogger<Index> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet() // vai ser sempre executado quando a pagina estiver sendo renderizada
+        {
+        }
+
+        public void OnPost()
+        {
+        }
+
+    }
+}
